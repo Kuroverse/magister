3ba370a72dc018f28365f1792258d005c47ba6f6
Kuroverse <124959533+Kuroverse@users.noreply.github.com>
2022-04-05 09:50:19 +0000

    Linter fixes

--- generate_table.py
+++ generate_table.py
@@ -4,4 +4,16 @@ import xml.etree.ElementTree as etree
 table = etree.Element("table", attrib={
     "id": "apple",
     "class": "tablesorter",
-    "cellspacing": "1
+    "cellspacing": "1",
+    "cellpadding": "0",
+    })
+
+thead = etree.SubElement(table, "thead")
+tbody = etree.SubElement(table, "tbody")
+tr = etree.SubElement(thead, "tr")
+
+for heading in ["Product", "Release Date", "Original Price", "Stock Value Today"]:
+    th = etree.SubElement(tr, "th")
+    th.text = heading
+
+for product in json.load(o
