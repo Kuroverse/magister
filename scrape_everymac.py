6b57a7da327fcc6e277dca5b71b5c59f3fd1fa9e
Kuroverse <124959533+Kuroverse@users.noreply.github.com>
2022-12-13 14:51:15 +0000

    Update step.yml

--- scrape_everymac.py
+++ scrape_everymac.py
@@ -6,4 +6,19 @@ import urllib
 import urllib2
 from BeautifulSoup import BeautifulSoup
 import re, htmlentitydefs
-import urlpar
+import urlparse
+import os
+import pprint
+import json
+import HTMLParser
+
+def slugify(string):
+    string = re.sub('\s+', '-', string)
+    string = re.sub('[\._]', '-', string)
+    string = re.sub('[^\w.-]', '', string)
+    return string.strip('_.- ').lower()
+    
+def unescape(text):
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] ==
